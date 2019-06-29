4c08f185af6a951a3ea07b935f0e42f0b630967b
wangjungang <1499370499@qq.com>
2019-06-29 15:06:53 +0800

    add_personVM

---
+++ HomepersonViewModel.h
@@ -0,0 +1,18 @@
+//
+//  HomepersonViewModel.h
+//  圣魔无界
+//
+//  Created by 王俊钢 on 2019/6/29.
+//  Copyright © 2019 a. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "infoModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomepersonViewModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HomepersonViewModel.m
@@ -0,0 +1,13 @@
+//
+//  HomepersonViewModel.m
+//  圣魔无界
+//
+//  Created by 王俊钢 on 2019/6/29.
+//  Copyright © 2019 a. All rights reserved.
+//
+
+#import "HomepersonViewModel.h"
+
+@implementation HomepersonViewModel
+
+@end
